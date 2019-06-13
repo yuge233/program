402af77a2e1c4b31d56a3f7136a07cf9f4b4e351
yuge233 <704453881@qq.com>
2019-06-14 00:05:05 +0800

    修改模型类

--- src/main/java/cn/group/program/model/Describe.java
+++ src/main/java/cn/group/program/model/Describe.java
@@ -1,11 +1,43 @@
 package cn.group.program.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.io.Serializable;
 
 @Entity
-@Table(name = "describe")
-public class Describe {
-    private Long questionId;
+@Table(name = "describes")
+public class Describe implements Serializable {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "token")
     private String token;
+
+    @ManyToOne
+    private Question question;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Question getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
 }
--- src/main/java/cn/group/program/model/Question.java
+++ src/main/java/cn/group/program/model/Question.java
@@ -1,15 +1,45 @@
 package cn.group.program.model;
 
 import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
-@Table(name = "question")
-public class Question {
+@Table(name = "questions")
+public class Question implements Serializable {
     @Id
     @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "answer")
     private String answer;
+
+    @OneToMany(cascade = CascadeType.ALL,mappedBy = "question")
+    private Collection<Describe> describes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public Collection<Describe> getDescribes() {
+        return describes;
+    }
+
+    public void setDescribes(Collection<Describe> describes) {
+        this.describes = describes;
+    }
 }
